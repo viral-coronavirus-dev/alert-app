72e033ecac8edded8be723e269fb855971ad9a5d
Matteo De Micheli <matteodem@protonmail.com>
2020-02-03 18:51:52 +0100

    Add workflow logic

--- imports/ui/steps/SymptomsStep.tsx
+++ imports/ui/steps/SymptomsStep.tsx
@@ -13,11 +13,15 @@ export default function SymptomsStep(props) {
                     <div key={key} className={"inline-block w-1/4"}>
                         <div className={"block cursor-pointer"} onClick={() => props.onClick(key, value)}>
                             <div className={`
-                                        ${value.value ? 'bg-green-500 text-white' : 'bg-white text-green-500'} 
-                                        hover:bg-green-500 hover:text-white rounded-full mr-3 mx-auto
+                                        ${value.value ? 'bg-green-500 text-white active-icon-wrapper' : 'bg-white text-green-500'} 
+                                        hover:bg-green-500 hover:text-white rounded-full mr-3 mx-auto hover-icon-wrapper
                                     `}
                                  style={{ width: "150px", height: "150px", fontSize: "82px", paddingTop: "14px" }}>
-                                <i className={"fas fa-thermometer"}></i>
+                                <div className={"relative"} style={{ maxWidth: "90px", margin: "0 auto", top: "16px"}}>
+                                    <img className={"absolute top-0 left-0 icon"} src={`/icons/${value.icon}.svg`} />
+                                    <img className={`absolute top-0 left-0 icon-active`}
+                                         src={`/icons/${value.icon}_active.svg`} />
+                                </div>
                             </div>
                             <div className={"font-light text-lg"}>
                                 {value.title}
