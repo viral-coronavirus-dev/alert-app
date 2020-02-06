57a91b6aac4023889b8d653c3d4ca1d9c6fc6440
muditha <muditha@paragonsoft.net>
2020-02-06 17:53:03 +0530

    phone number valdiation with react-phone-number-input
    phone number valdiation with react-phone-number-input

--- imports/ui/steps/PhoneNumberStep.tsx
+++ imports/ui/steps/PhoneNumberStep.tsx
@@ -1,19 +1,31 @@
-import React from 'react'
-import StepTitle from './StepTitle'
-import Input from '../components/Input'
+import React from "react";
+import StepTitle from "./StepTitle";
+//import Input from "../components/Input";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
 
 export default function PhoneNumberStep(props) {
-    return (
-        <div className={"text-center mx-auto step-wrapper"}>
-            <StepTitle>Please enter your phone number</StepTitle>
+  return (
+    <div className={"text-center mx-auto step-wrapper"}>
+      <StepTitle>Please enter your phone number</StepTitle>
 
-            <div className={"flex"}>
-                <form className={"w-full"} onSubmit={e => {
-                    e.preventDefault()
-                }}>
-                    <Input type={"text"} placeholder={"Phone number"} onChange={(e) => props.onChange(e.target.value)} />
-                </form>
-            </div>
-        </div>
-    );
+      <div className={"flex"}>
+        <form
+          className={"w-full"}
+          onSubmit={e => {
+            e.preventDefault();
+          }}
+        >
+          <PhoneInput
+            type={"text"}
+            className={"w-full px-3 py-2 lg:p-3 text-sm lg:text-base placeholder-uppercase rounded border-solid border border-black"}
+            placeholder={"Phone number"}
+            onChange={phone => {
+              props.onChange(phone);
+            }}
+          />
+        </form>
+      </div>
+    </div>
+  );
 }
