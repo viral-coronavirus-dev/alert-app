ee504fc0a14e296c444bf6b05409b5f9492f5173
muditha <muditha@paragonsoft.net>
2020-02-07 12:10:31 +0530

    react alerts
    react alert on invalid auth code

--- client/main.tsx
+++ client/main.tsx
@@ -1,9 +1,24 @@
-import 'core-js'
-import React from 'react'
-import { Meteor } from 'meteor/meteor'
-import { render } from 'react-dom'
-import App from '/imports/ui/App'
+import "core-js";
+import React from "react";
+import { Meteor } from "meteor/meteor";
+import { render } from "react-dom";
+import App from "/imports/ui/App";
+import { transitions, positions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('react-target'))
-})
+  const options = {
+    position: positions.TOP_CENTER,
+    timeout: 3000,
+    offset: "30px",
+    transition: transitions.FADE
+  };
+  const Root = () => (
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
+  );
+
+  render(<App />, document.getElementById("react-target"));
+  render(<Root />, document.getElementById("root"));
+});
--- private/twilio/create-verify-service.js
+++ private/twilio/create-verify-service.js
@@ -4,5 +4,5 @@ const authToken = credentials.auth_token
 
 const client = require('twilio')(accountSid, authToken)
 
-client.verify.services.create({friendlyName: 'Viral Corona Virus App'})
+client.verify.services.create({ friendlyName: 'Viral Corona Virus App' })
   .then(service => console.log(JSON.stringify(service)))
